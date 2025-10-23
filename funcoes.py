c8a8012ff7a1925505976a277da2b972c0cec9f4
Paulo Pereira <paulopos@al.insper.edu.br>
2025-10-23 18:27:17 -0300

    Criei a funcao define_posicoes

---
+++ funcoes.py
@@ -0,0 +1,9 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    #posicoes = [[linha, coluna]]*tamanho
+    posicoes = []
+    for i in range(tamanho):
+        if orientacao == 'horizontal':
+            posicoes.append([linha,coluna+i])
+        else:
+            posicoes.append([linha+i, coluna])
+    return posicoes
