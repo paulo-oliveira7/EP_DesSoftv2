a7c35468eb3e6bcf00fa7b6222f8067dd1486433
Paulo Pereira <paulopos@al.insper.edu.br>
2025-10-24 01:07:08 -0300

    Criei a funcao faz_jogada

--- funcoes.py
+++ funcoes.py
@@ -17,3 +17,11 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     else:
         frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
     return frota
+
+# Função da jogada
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    elif tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    return tabuleiro
